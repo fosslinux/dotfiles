c4450149e43b555d7a25472a897b2fb6d9eec6a2
fosslinux <fosslinux@aussies.space>
2019-12-27 18:18:12 +1100

    typo

--- install.sh
+++ install.sh
@@ -43,7 +43,7 @@ done
 
 # Install oh-my-zsh 
 printf "Install oh-my-zsh\\n"
-if_exists "${PREFIX}/.oh-my-zsh"
+if_exists ".oh-my-zsh"
 ZSH="${PREFIX}/.oh-my-zsh" ./oh-my-zsh-install.sh
 printf "\\nInstall oh-my-zsh spaceship theme\\n"
 git clone https://github.com/denysdovhan/spaceship-prompt.git "${PREFIX}/.oh-my-zsh/themes/spaceship-prompt"
